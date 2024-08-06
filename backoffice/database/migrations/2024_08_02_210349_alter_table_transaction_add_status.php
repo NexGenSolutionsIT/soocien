ea48f5049127b2895d893f39b4f636ee713cf53c
Alexandre Osovski <alexandreosovski1@gmail.com>
2024-08-05 21:58:17 -0300

    fix: adjusts in received webhook pix

--- backoffice/database/migrations/2024_08_02_210349_alter_table_transaction_add_status.php
+++ backoffice/database/migrations/2024_08_02_210349_alter_table_transaction_add_status.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('transactions', function (Blueprint $table) {
-            $table->enum('status', ['approved', 'cancel', 'waiting_approval'])->default('waiting_approval');
             $table->string('external_reference')->default("NULL");
         });
     }
